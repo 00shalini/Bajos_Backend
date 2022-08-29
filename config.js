4c01c3fdc32d0fd9d5e6acc5bca8afc36b65b846
00shalini <shalini07061997@gmsail.com>
2022-08-29 18:26:30 +0530

    updated

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const firebase = require('firebase');
-require('dotenv').config();
+const firebase = require("firebase");
+
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: "AIzaSyCPZ6r8PhcU6JIx_-qG165iQbgsWAOSlsQ",
+  authDomain: "bajos-52e28.firebaseapp.com",
+  projectId: "bajos-52e28",
+  storageBucket: "bajos-52e28.appspot.com",
+  messagingSenderId: "140253716361",
+  appId: "1:140253716361:web:3fb871a233315894104b58",
+  measurementId: "G-HNH43XJZ9T",
 };
 
 const db = firebase.initializeApp(firebaseConfig);
